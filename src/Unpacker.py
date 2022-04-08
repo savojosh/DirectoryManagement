4f70c70248e6cfe2caa0b0463bfbd14660b84ce8
axonon <jmps2022@gmail.com>
2022-04-08 16:22:51 -0500

    Setup for the Unpacker 16:22
    Plan: Unzips and decompresses all zipped and compressed files within a directory.

---
+++ src/Unpacker.py
@@ -0,0 +1,15 @@
+# get the directory
+# list the directory's contents
+# unzip/decompress all contents that are .zip, .tar.gz, .tgz, .tar, and .7z
+
+# extract tar
+# extract zip
+
+
+# Imports
+
+import os
+import sys
+import zipfile
+import tarfile
+
